d8a6e11873d885a5b65382a921e0fdd066318794
Jose Octavio de Castro Neves Jr <joc@fullsquad.com.br>
2020-01-28 16:05:56 -0300

    deploy build v0.1

---
+++ public/precache-manifest.94b5345b3603de408e4e79439e24f7dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69158e8b68271dd835b06be829389ba9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1c5e8c0ce306bea7c9f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1376d9aff12f804f8447",
+    "url": "/static/js/2.e98dbd9a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e98dbd9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1c5e8c0ce306bea7c9f",
+    "url": "/static/js/main.bf436298.chunk.js"
+  },
+  {
+    "revision": "0882cfeb955535936e80",
+    "url": "/static/js/runtime-main.39b776dc.js"
+  }
+]);
